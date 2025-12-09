d28323931ed5ebfbb2156394fe9cfd75a5748f57
hfzfjr <hafizfajar0101@gmail.com>
2025-12-09 13:31:48 +0700

    feat : money logic

--- MINGGU 1/club-coding.js
+++ MINGGU 1/club-coding.js
@@ -6,4 +6,11 @@ let sisaUmur = 21 - umur;
 
 if (umur < 21) {
     console.log(`Maaf ${nama}, umur kamu kurang ${sisaUmur} tahun lagi`);
+} else if (umur >= 21) {
+    const uang = parseInt(prompt("masukkan jumlah uang anda: "), 10);
+    if (uang < 500000) {
+        console.log(`Maaf ${nama}, uang kamu cuma ${uang} ribu, datang lagi lain kali`);
+    } else {
+        console.log(`Selamat datang ${nama}`);
+    }
 }
